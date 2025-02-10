c7157a545ebfc9006fda6266585ec5e63bb160bc
Ricky Ng-Adam <ricky@coderbunker.ca>
2025-02-10 00:06:32 -0500

    add tailscale connectivity with key in secrets

--- flake.nix
+++ flake.nix
@@ -4,14 +4,37 @@
   inputs = {
     # NixOS official package source, using the nixos-24.11 branch here
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    agenix.url = "github:ryantm/agenix";
     home-manager = {
       url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, agenix, ... }@inputs: {
     nixosConfigurations.nixos02 = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        # Import the previous configuration.nix we used,
+        # so the old configuration file still takes effect
+        ./configuration.nix
+        ./tailscale.nix
+        home-manager.nixosModules.home-manager 
+	{
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.verbose = true;
+            home-manager.users.rngadam = import ./home.nix;
+            home-manager.backupFileExtension = "bak";
+	}
+        agenix.nixosModules.default
+        {
+            environment.systemPackages = [ agenix.packages.x86_64-linux.default ];
+        }
+      ];
+    };
+
+    nixosConfigurations.nixos01 = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         # Import the previous configuration.nix we used,
