a475bdc4e578232db11ac710928427c58eb4a833
Ricky Ng-Adam <ricky@coderbunker.ca>
2025-02-20 05:45:58 -0500

    persist group lxc_shares

--- virtiofs.nix
+++ virtiofs.nix
@@ -1,6 +1,10 @@
 { config, pkgs, ... }:
 
 {
+    users.groups.lxc_shares = {
+        gid = 10100;
+        members = [ "rngadam" ];
+    };
     fileSystems."/media" = {
         device = "108-media";
         fsType = "virtiofs";
